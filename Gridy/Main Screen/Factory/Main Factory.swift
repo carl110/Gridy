bcf8f10210982a6ab4e41a6e5d618d51a9cd09a2
Carl Wainwright <carljame7@gmail.com>
2018-08-27 09:52:22 +0100

    Updated folder structure

---
+++ Gridy/Main Screen/Factory/Main Factory.swift
@@ -0,0 +1,31 @@
+//
+//  Main Factory.swift
+//  Gridy
+//
+//  Created by Carl Wainwright on 27/08/2018.
+//  Copyright © 2018 Carl Wainwright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainFactory {
+    
+    static func showIn(window: UIWindow) {
+        
+        let navigationController = UINavigationController()
+        navigationController.isNavigationBarHidden = true
+        navigationController.navigationBar.backgroundColor = UIColor.lightGray
+        navigationController.navigationBar.tintColor = UIColor.blue
+        navigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.blue]
+        
+        let mainController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController
+        let mainFlowController = MainFlowController(navigationController: navigationController)
+        
+        mainController.assignDependencies(mainFlowController: mainFlowController)
+        
+        navigationController.setViewControllers([mainController], animated: false)
+        window.rootViewController = navigationController
+        
+    }
+}
---
+++ Gridy/Main Screen/Flow Controller/Main Flow Controller.swift
@@ -0,0 +1,24 @@
+//
+//  Main Flow Controller.swift
+//  Gridy
+//
+//  Created by Carl Wainwright on 27/08/2018.
+//  Copyright © 2018 Carl Wainwright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainFlowController {
+    
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func showEditor(with photo: UIImage) {
+    
+    }
+    
+}
