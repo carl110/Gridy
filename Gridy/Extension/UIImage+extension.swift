2406076957b9b35cf929d81e6aeabd9ec07c7d2d
Carl Wainwright <carljame7@gmail.com>
2018-09-24 12:53:23 +0100

    added scale to split image extension to get correct sizing for the image array

--- Gridy/Extension/UIImage+extension.swift
+++ Gridy/Extension/UIImage+extension.swift
@@ -12,8 +12,8 @@ import UIKit
 extension UIImage {
     
     func splitImage(_ rows: Int, _ columns: Int) -> [UIImage] {
-        let y = (size.height / CGFloat(rows))
-        let x = (size.width / CGFloat(columns))
+        let y = scale * (size.height / CGFloat(rows))
+        let x = scale * (size.width / CGFloat(columns))
         var images: [UIImage] = []
         images.reserveCapacity(rows * columns)
         guard let cgImage = cgImage else { return [] }
@@ -22,33 +22,10 @@ extension UIImage {
                 var width = Int(x)
                 var height = Int(y)
                 if row == rows-1 && size.height.truncatingRemainder(dividingBy: CGFloat(rows)) != 0 {
-                    height = Int(size.height - size.height / CGFloat(rows) * (CGFloat(rows)-1))
+                    height = Int(scale * (size.height - size.height / CGFloat(rows) * (CGFloat(rows)-1)))
                 }
                 if column == columns-1 && size.width.truncatingRemainder(dividingBy: CGFloat(columns)) != 0 {
-                    width = Int(size.width - (size.width / CGFloat(columns) * (CGFloat(columns)-1)))
-                }
-                if let image = cgImage.cropping(to: CGRect(origin: CGPoint(x: column * Int(x), y:  row * Int(x)), size: CGSize(width: width, height: height))) {
-                    images.append(UIImage(cgImage: image, scale: scale, orientation: imageOrientation))
-                }
-            }
-        }
-        return images
-    }
-    func matrix(_ rows: Int, _ columns: Int) -> [UIImage] {
-        let y = (size.height).rounded()
-        let x = (size.width).rounded()
-        var images: [UIImage] = []
-        images.reserveCapacity(rows * columns)
-        guard let cgImage = cgImage else { return [] }
-        (0..<rows).forEach { row in
-            (0..<columns).forEach { column in
-                var width = Int(x)
-                var height = Int(y)
-                if row == rows-1 && size.height.truncatingRemainder(dividingBy: CGFloat(rows)) != 0 {
-                    height = Int(size.height - size.height / CGFloat(rows) * (CGFloat(rows)-1))
-                }
-                if column == columns-1 && size.width.truncatingRemainder(dividingBy: CGFloat(columns)) != 0 {
-                    width = Int(size.width - (size.width / CGFloat(columns) * (CGFloat(columns)-1)))
+                    width = Int(scale * (size.width - (size.width / CGFloat(columns) * (CGFloat(columns)-1))))
                 }
                 if let image = cgImage.cropping(to: CGRect(origin: CGPoint(x: column * Int(x), y:  row * Int(x)), size: CGSize(width: width, height: height))) {
                     images.append(UIImage(cgImage: image, scale: scale, orientation: imageOrientation))
